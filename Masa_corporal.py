39c06ff47db46bb47fa4a6e7197e89f64df98959
Aprendiz - Allison Michele <allison_mrh@misena.edu.co>
2025-08-01 16:05:17 -0500

    nuevo archivo

---
+++ Masa_corporal.py
@@ -0,0 +1,8 @@
+peso = float(input("Digite su peso en Kg: "))#pido los datos al usuario
+print("su Peso es "+ str(peso)+ "kg")
+altura =float(input("Digite su altura en metros: "))
+print("Su Altura es"+ str(altura) +"m")
+#Resultado
+resultado = (peso+altura)
+print("Su masa Corporal es "+str(resultado)+ " ")
+
---
+++ mayoria_de_edad.py
@@ -0,0 +1,9 @@
+edad = float(input("Ingrese su edad: "))
+# Operación
+if edad < 18:
+    print("Menor de edad")
+elif edad >= 18 and edad < 25:
+    print("Joven adulto")
+else:
+    print("adulto")
+
